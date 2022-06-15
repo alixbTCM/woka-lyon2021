5064459bdb4605c253dc005abc3fa546dae05cda
morgane <m.huebra@thecodingmachine.com>
2022-06-15 13:39:32 +0200

    Lady of the lake

--- my-script.js
+++ my-script.js
@@ -12,6 +12,8 @@ const cavernCeiling = [
     'Waterfall'
 ]
 
+let LadyCounter = 0;
+
 
 // Rabbit Hole zone
 WA.room.onEnterLayer("rabbitHoleZone").subscribe(() => {
@@ -38,3 +40,31 @@ WA.room.onLeaveLayer("cavernZone").subscribe(() => {
         WA.room.showLayer(cavernCeiling[i])
     }
 });
+
+// Lady of the lake
+WA.room.onEnterLayer("ladyOfTheLakeZone").subscribe(() => {
+    if (LadyCounter === 0) {
+        WA.chat.sendChatMessage("Halte-là !", "Dame du lac")
+        WA.chat.sendChatMessage("Vous n'êtes pas dignes d'un tel honneur !", "Dame du lac")
+        WA.chat.sendChatMessage("Seul les preux chevaliers ayant bravé les dangers d'Avalon peuvent prétendre devenir rois en retirant l'épée !", "Dame du lac")
+    }
+
+    if (LadyCounter === 1) {
+        WA.chat.sendChatMessage("VOUS ne passerez PAS !", "Dame du lac")
+    }
+
+    if (LadyCounter === 2) {
+        WA.chat.sendChatMessage("Je t'ai dit d'aller à AVALON !", "Dame du lac")
+    }
+
+    if (LadyCounter > 2) {
+        WA.chat.sendChatMessage("Tu ne vas jamais me laisser tranquille en fait ? ...", "Dame du lac")
+    }
+
+    WA.room.showLayer('Lady-of-the-lake')
+    LadyCounter ++
+})
+
+WA.room.onLeaveLayer("ladyOfTheLakeZone").subscribe(() => {
+    WA.room.hideLayer('Lady-of-the-lake')
+})
