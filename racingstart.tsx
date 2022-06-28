ba7fd688d9c99679e59048bc587b3b1c600d233a
ALB <a.boufflers@thecodingmachine.com>
2022-06-28 13:16:05 +0200

    water test + racing test

---
+++ racingstart.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="racingstart" tilewidth="32" tileheight="32" tilecount="5" columns="5">
+ <image source="src/assets/racingstart.png" width="160" height="32"/>
+</tileset>
