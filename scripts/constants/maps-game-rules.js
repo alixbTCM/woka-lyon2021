91b8ee3004f1ff3e2b51813bfb18a821b15db3f9
morgane <m.huebra@thecodingmachine.com>
2022-06-28 13:16:17 +0200

    Aucune idée

---
+++ scripts/constants/maps-game-rules.js
@@ -0,0 +1,16 @@
+const graalMapGameRules = [
+    'Vous devez trouver le saint Graal parmi les objets exposés dans la salle.',
+    'De nombreux chevaliers s\'y sont essayés et ont échoué.',
+    'En examinant la salle, peut-être trouverez vous des indices !'
+]
+
+const escapeMapeGameRules = [
+    'Bienvenue dans le chateau du roi Pêcheur !',
+    'Afin de poursuivre votre quête, vous devez réussir à vous échapper de cette salle.',
+    'Bonne chance !'
+]
+
+export {
+    graalMapGameRules,
+    escapeMapeGameRules
+}
