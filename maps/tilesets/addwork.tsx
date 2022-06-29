e7072df8d267427d28486edb043764eda681c81c
morgane <m.huebra@thecodingmachine.com>
2022-06-29 09:28:00 +0200

    tilesets in right folder

--- maps/tilesets/addwork.tsx
+++ maps/tilesets/addwork.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="addwork" tilewidth="32" tileheight="32" tilecount="768" columns="12">
- <image source="src/assets/addwork.png" width="384" height="2048"/>
+ <image source="../../src/assets/addwork.png" width="384" height="2048"/>
 </tileset>
