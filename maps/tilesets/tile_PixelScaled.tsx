6a7a94a5558e7cf4cf8229d9dbacb6d32cf0ebc7
morgane <m.huebra@thecodingmachine.com>
2022-06-29 13:37:02 +0200

    escape map ordered file

--- maps/tilesets/tile_PixelScaled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="tile_PixelScaled" tilewidth="32" tileheight="32" tilecount="576" columns="24">
- <image source="../../tile_PixelScaled.png" width="768" height="768"/>
-</tileset>
