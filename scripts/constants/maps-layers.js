7bd928a7435d108897ac6d10f2d6cdc593ca08cf
morgane <m.huebra@thecodingmachine.com>
2022-07-28 12:29:02 +0200

    cascade door

--- scripts/constants/maps-layers.js
+++ scripts/constants/maps-layers.js
@@ -1,6 +1,6 @@
 const principalMapLayers = {
     rabbitHoleCeilings:  ['Ceiling-alice-1', 'Ceiling-alice-2', 'Ceiling-alice-3', 'Ceiling-alice-0', 'Ceiling-alice-01'],
-    cavernCeiling: ['Ceiling-cave-2', 'Ceiling-cave-1', 'Waterfall'],
+    cavernCeiling: ['Ceiling-cave-2', 'Ceiling-cave-1', 'WaterfallOpenedUp', 'WaterfallOpenedDown', 'WaterfallClosedUp', 'WaterfallClosedDown'],
     ladyOfTheLake: ['Lady-of-the-lake', 'Lady-of-the-lake-up'],
     meetings: ['caverne', 'terrier-du-lapin', 'champ', 'place', 'quais', 'black-pearl']
 }
